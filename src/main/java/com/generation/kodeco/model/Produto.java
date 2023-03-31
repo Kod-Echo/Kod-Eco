b38a3caa4f140709cbf2369240eac659d7cb08a9
Guilherme Amorim <guilhermedeamorim098@gmail.com>
2023-03-31 16:10:56 -0300

    Atualizacao 0.0.1 - Usuario

--- src/main/java/com/generation/kodeco/model/Produto.java
+++ src/main/java/com/generation/kodeco/model/Produto.java
@@ -97,4 +97,12 @@ public class Produto {
 		this.categoria = categoria;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 }
